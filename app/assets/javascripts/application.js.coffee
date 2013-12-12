56dbb53929b244f6115f09eef86eea635600952c
ogawakeisuke <ogawa@uniba.jp>
2013-12-12 14:20:54 +0900

    first

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,9 @@
+#= require handlebars
+#= require ember
+#= require ember-data
+#= require_self
+#= require ember_rails_test
+
+# for more details see: http://emberjs.com/guides/application/
+window.EmberRailsTest = Ember.Application.create()
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberRailsTest::Application.config.secret_key_base = '6a3c3cfcb1117225328f38e2072f64d4d882c7f4f72eb0577ece966dc57afc37ab70132da55774769e73d9f45ceeb47389c716d4c43b04f801ce34b431181a08'
