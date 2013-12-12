844efd751ce11454501126f518e5f832cc728def
ogawakeisuke <ogawa@uniba.jp>
2013-12-12 15:25:26 +0900

    require解決か？

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ember_rails_test
 //= require_tree .
+
+//for more details see: http://emberjs.com/guides/application/
+
+
+
